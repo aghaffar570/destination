e4ee216779e02a6fca6d991d98929502c380b23a
Abdul Ghaffar <aghaffar570@gmail.com>
2019-02-16 12:36:05 -0500

    database configured with heroku

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const pkg = require('../../package.json');
 const databaseName = pkg.name;
 
-const db = new Sequelize(`postgres://localhost:5432/${databaseName}`, {
+const db = new Sequelize(`postgres://jhotetiodigsfa:f7c1348384f1c1dec34785a410f6f4925baf9823851363c51d6261b72b0a096c@ec2-54-163-234-88.compute-1.amazonaws.com:5432/di504v7mdcbvp`, {
   logging: false
 });
 
